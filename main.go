95f924770f1e1d878026ad106ac577f6057b0657
devgenie <jonen54@gmail.com>
2023-08-25 00:13:44 +0200

    start brender server

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"brender/api/router"
+	"brender/config"
+	"brender/util/logger"
+	"brender/util/validator"
+)
+
+func main() {
+	c := config.New()
+	l := logger.New(c.Server.Debug)
+	v := validator.New()
+
+	r := router.New(l, v)
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf(":%d", c.Server.Port),
+		Handler:      r,
+		ReadTimeout:  c.Server.TimeoutRead,
+		WriteTimeout: c.Server.TimeoutWrite,
+		IdleTimeout:  c.Server.TimeoutIdle,
+	}
+
+	closed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+		<-sigint
+
+		l.Info().Msgf("Shutting down server %v", s.Addr)
+
+		ctx, cancel := context.WithTimeout(context.Background(), c.Server.TimeoutIdle)
+		defer cancel()
+
+		if err := s.Shutdown(ctx); err != nil {
+			l.Error().Err(err).Msg("Server shutdown failure")
+		}
+
+		close(closed)
+	}()
+
+	l.Info().Msgf("Starting server %v", s.Addr)
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		l.Fatal().Err(err).Msg("Server startup failure")
+	}
+
+	<-closed
+}
