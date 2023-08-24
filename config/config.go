ea91597ac96ca37f0e7b51a8ae07e43d9241eac8
devgenie <jonen54@gmail.com>
2023-08-25 00:13:02 +0200

    Add config boilerplate

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/joeshaw/envdecode"
+)
+
+type Conf struct {
+	Server ConfServer
+}
+
+type ConfServer struct {
+	Port         int           `env:"SERVER_PORT,required"`
+	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ,required"`
+	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
+	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
+	Debug        bool          `env:"SERVER_DEBUG,required"`
+}
+
+func New() *Conf {
+	var c Conf
+	if err := envdecode.StrictDecode(&c); err != nil {
+		log.Fatalf("Failed to decode: %s", err)
+	}
+
+	return &c
+}
