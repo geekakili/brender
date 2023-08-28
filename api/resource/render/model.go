c863d7eb2f9d6b85ef128e0e718917adf1505964
devgenie <jonen54@gmail.com>
2023-08-29 01:17:47 +0200

    Validate form values from string input

--- api/resource/render/model.go
+++ api/resource/render/model.go
@@ -1,13 +1,13 @@
 package render
 
 type Form struct {
-	StartFrame      int    `json:"start_frame" form:"default, 1"`
-	EndFrame        int    `json:"end_frame" form:"default, 1"`
-	FrameJump       int    `json:"frame_jump" form:"default, 0"`
+	StartFrame      int    `json:"start_frame,string"`
+	EndFrame        int    `json:"end_frame,string"`
+	FrameJump       int    `json:"frame_jump,string"`
 	RenderFrames    string `json:"render_frames"`
 	RenderEngine    string `json:"render_engine"`
 	OutputFormat    string `json:"output_format"`
-	RenderAnimation bool   `json:"render_animation" form:"default, false"`
+	RenderAnimation bool   `json:"render_animation,string"`
 }
 
 type RenderObject struct {
